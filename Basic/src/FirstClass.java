1cafceeb69be244e0175d9340aad67005313bb0d
Na Lin <lna483018@gmail.com>
2023-04-26 23:17:27 -0400

    print with \n new line

--- Basic/src/FirstClass.java
+++ Basic/src/FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello Na");
+        System.out.println("Hello World");
+        System.out.println("Hi there. ");
     }
 }
