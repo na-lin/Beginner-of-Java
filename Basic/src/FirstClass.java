9850679078f837b67bc706d0866eacff3fa233bf
Na Lin <lna483018@gmail.com>
2023-04-27 23:36:52 -0400

    practice java variable

--- Basic/src/FirstClass.java
+++ Basic/src/FirstClass.java
@@ -1,6 +1,9 @@
 public class FirstClass {
     public static void main(String[] args) {
+        int num1 = 12;
+        char char1 = 'a';
+        double d1 = 5.2;
         System.out.println("Hello World");
-        System.out.println("Hi there. ");
+        System.out.println("Java Variable" +  num1 +  char1 + d1);
     }
 }
