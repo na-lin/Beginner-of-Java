5622b131a0dba9da8faa50e90e23da3af601a34b
Na Lin <lna483018@gmail.com>
2023-04-28 23:55:59 -0400

    casting

--- Basic/src/FirstClass.java
+++ Basic/src/FirstClass.java
@@ -3,6 +3,7 @@ public class FirstClass {
         int num1 = 12;
         char char1 = 'a';
         double d1 = 5.2;
+        short s1 = (short)12;
         System.out.println("Hello World");
         System.out.println("Java Variable" +  num1 +  char1 + d1);
     }
