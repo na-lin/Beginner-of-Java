c0a3ed713a817cfc7645e5300d0a6926be144af8
Na Lin <lna483018@gmail.com>
2023-05-03 01:37:23 -0400

    condition in java

--- Basic/src/FirstClass.java
+++ Basic/src/FirstClass.java
@@ -1,10 +1,20 @@
 public class FirstClass {
     public static void main(String[] args) {
-        int num1 = 12;
-        char char1 = 'a';
-        double d1 = 5.2;
-        short s1 = (short)12;
-        System.out.println("Hello World");
-        System.out.println("Java Variable" +  num1 +  char1 + d1);
+        String greeting = "hello world";
+        System.out.println(greeting);
+
+        boolean isPrint = true;
+        if(isPrint == false){
+            System.out.println("this is print");
+        }else{
+            System.out.println("Don't print");
+        }
+        int bit = isPrint ? 1 : 0;
+        System.out.println(bit);
+
+//challenge
+        double v1 = 20.00;
+        double v2 = 80.00;
+        System.out.println(v1);
     }
 }
