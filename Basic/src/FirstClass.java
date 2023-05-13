48732997536d09159a09c92423da25009f9b3913
Na Lin <lna483018@gmail.com>
2023-05-13 17:18:38 -0400

    Method in Java

--- Basic/src/FirstClass.java
+++ Basic/src/FirstClass.java
@@ -1,22 +1,9 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String greeting = "hello world";
-        System.out.println(greeting);
+        greeting();
+    }
 
-        boolean isPrint = true;
-        if(isPrint == false){
-            System.out.println("this is print");
-        }else{
-            System.out.println("Don't print");
-        }
-        int bit = isPrint ? 1 : 0;
-        System.out.println(bit);
-
-//challenge
-        double v1 = 20.00;
-        double v2 = 80.00;
-        System.out.println(v1);
-        boolean b1 = v1 == v2 ? true: false;
-        System.out.println("b1:"+ b1);
+    public static void greeting(){
+        System.out.println("Hello World!");
     }
 }
